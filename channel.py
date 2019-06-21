25d6423500a148efa8af48a904d4ff5141eb4088
Connor Zanin <cnnrznn@gmail.com>
2019-06-21 11:33:28 -0400

    comments

--- channel.py
+++ channel.py
@@ -1,5 +1,10 @@
 class Channel:
 
+    # Notes: The send processes must be killable. If they get stuck trying to
+    # deliver messages to byzantine processes they may hang indefinitely. Create
+    # a function to clean them up or destroy them, possible selectively (for
+    # generality), or in one big KILLALL syle function.
+
     def __init__(self, conf, mf):
         self.conf = conf
         self.filter = mf
@@ -11,6 +16,11 @@ class Channel:
         # 1. spin off new thread
         # 2. get my own socket for sending
         # 3. wait with timeouts for response and resend
+
+        # NOTE: I need to wait for EVERY of the 3f + 1 processes to respond. If
+        # not, it is possible less than 2f+1 correct processes received the
+        # initial, which means there won't be enought ECHO's "in the system" in
+        # order to accept the echo.
         pass
 
     def recv(self):
