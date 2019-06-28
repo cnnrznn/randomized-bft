abe7daa933d014106d1f2a186d7b78e8d941be09
Connor Zanin <cnnrznn@gmail.com>
2019-06-28 07:46:15 -0400

    spacing, testing

--- src/message.py
+++ src/message.py
@@ -1,9 +1,12 @@
 import enum
 import json
 
+
+
 class Type(enum.IntEnum):
     INITIAL = 1
     ECHO    = 2
+
 
 
 def from_bytes(b):
@@ -15,13 +18,27 @@ def from_bytes(b):
                    d['value'],
                    d['round'])
 
+
+
 class Message:
+
+
 
     def __init__(self, ty, fr, v, r):
         self.type = ty
         self.fr = fr
         self.value = v
         self.round = r
+
+
+
+    def __str__(self):
+        return '({}, {}, {}, {})'.format(self.type,
+                                         self.fr,
+                                         self.value,
+                                         self.round)
+
+
 
     def to_bytes(self):
         d = { 'type' : self.type,
@@ -33,5 +50,10 @@ class Message:
 
         return s.encode()
 
+
+
 if __name__ == '__main__':
-    Message(Type.INITIAL, 'me', 4, 0)
+    msg = from_bytes(Message(Type.ECHO, 'me', 4, 0).to_bytes())
+    print(msg)
+    print(Type.INITIAL == msg.type)
+    print(Type.ECHO == msg.type)
